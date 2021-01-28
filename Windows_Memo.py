41bbae12984ba41c81f7cf3f7fbb332c950e2425
JoSangYeon <whtkddus98@kookmin.ac.kr>
2021-01-28 15:14:47 +0900

    윈도우즈 메모장 구현

--- Windows_Memo.py
+++ Windows_Memo.py
@@ -1,67 +1,88 @@
 from tkinter import *
+import os
 
-"""메모장 main 창 생성"""
-root = Tk()
-root.title("제목없음 - Windows 메모장")
-root.geometry("800x600")
-
-"""스크롤 바"""
-scroll = Scrollbar(root)
-scroll.pack(side="right", fill="y")
-"""텍스트 위젯"""
-txt = Text(root) #텍스트 위젯 생성
-txt.pack(fill="both", expand=True)
-txt.config(yscrollcommand=scroll.set)
-"""텍스트 위젯"""
-scroll.config(command=txt.yview)
-"""스크롤 바"""
-
-"""상단 메뉴 구현"""
-menu = Menu(root)
-
-def create_file():
+class Note:
     """
-    상단 메뉴의 파일->열기를 눌렀을때.
-    mynote.txt의 내용이 출력됨
+    Tkinter 모듈로 만든 메모장
     """
-    global txt
-    txt.delete("1.0", END) #기존의 내용 삭제
-    f = open("mynote.txt", "r")
-    for line in f.readlines():
-        txt.insert(END, line)
-    f.close
+    def __init__(self, root):
+        self.root = root
+        self. scroll = Scrollbar(self.root)
+        self.txt = Text(root)
+        self.__set_scorll()
+        self.__set_txt()
 
-def save_file():
+    def __set_txt(self):
+        self.txt.pack(fill="both", expand=True)
+        self.txt.config(yscrollcommand=self.scroll.set)
+    def __set_scorll(self):
+        self.scroll.pack(side="right", fill="y")
+        self.scroll.config(command=self.txt.yview)
+
+class Note_Menu():
     """
-    상단 메뉴의 파일->저장을 눌렀을 때,
-    화면에 입력한 문자열이 mynote.txt에 저장됨
+    Node class의 상단 메뉴
     """
-    global txt
-    content = txt.get("1.0", END)
-    f = open("mynote.txt", "w")
-    f.write(content)
-    f.close()
+    def __init__(self, root, txt):
+        self.root = root
+        self.menu = Menu(root)
+        self.filename = "mynote.txt"
+        self.txt = txt
+        self.__create_menu()
+        root.config(menu=self.menu)
 
-menu_file = Menu(menu, tearoff=0)
-menu_file.add_command(label="열기", command=create_file)
-menu_file.add_command(label="저장", command=save_file)
-menu_file.add_separator() #구분자
-menu_file.add_command(label="닫기", command=root.quit)
+    def __create_menu(self):
+        menu_file = Menu(self.menu, tearoff=0)
+        menu_file.add_command(label="열기", command=self.create_file)
+        menu_file.add_command(label="저장", command=self.save_file)
+        menu_file.add_separator()  # 구분자
+        menu_file.add_command(label="닫기", command=self.root.quit)
 
-menu.add_cascade(label="파일", menu=menu_file)
+        self.menu.add_cascade(label="파일", menu=menu_file)
 
-menu_edit = Menu(menu, tearoff=0)
-menu_format = Menu(menu, tearoff=0)
-menu_view = Menu(menu, tearoff=0)
-menu_help = Menu(menu, tearoff=0)
+        menu_edit = Menu(self.menu, tearoff=0)
+        menu_format = Menu(self.menu, tearoff=0)
+        menu_view = Menu(self.menu, tearoff=0)
+        menu_help = Menu(self.menu, tearoff=0)
 
-menu.add_cascade(label="편집", menu=menu_edit)
-menu.add_cascade(label="서식", menu=menu_format)
-menu.add_cascade(label="보기", menu=menu_view)
-menu.add_cascade(label="도움말", menu=menu_help)
-"""상단 메뉴 구현"""
+        self.menu.add_cascade(label="편집", menu=menu_edit)
+        self.menu.add_cascade(label="서식", menu=menu_format)
+        self.menu.add_cascade(label="보기", menu=menu_view)
+        self.menu.add_cascade(label="도움말", menu=menu_help)
 
-"""메모장 실행"""
-root.config(menu=menu)
-root.mainloop()
-"""메모장 실행"""
+    def create_file(self):
+        """
+        상단 메뉴의 파일->열기를 눌렀을때.
+        mynote.txt의 내용이 출력됨
+        """
+        self.txt.delete("1.0", END)  # 기존의 내용 삭제
+        if os.path.isfile(self.filename):
+            f = open(self.filename, "r", encoding="utf8")
+            for line in f.readlines():
+                self.txt.insert(END, line)
+            f.close()
+
+    def save_file(self):
+        """
+        상단 메뉴의 파일->저장을 눌렀을 때,
+        화면에 입력한 문자열이 mynote.txt에 저장됨
+        """
+        content = self.txt.get("1.0", END)
+        if os.path.isfile(self.filename):
+            f = open(self.filename, "w")
+            f.write(content)
+            f.close()
+
+def main():
+    """메모장 main 창 생성"""
+    root = Tk()
+    root.title("제목없음 - Windows 메모장")
+    root.geometry("800x600")
+
+    note = Note(root)
+    note_menu = Note_Menu(root, note.txt)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
