2423c64cab77b7e44628f5859c03b7bcb3d07046
JoSangYeon <whtkddus98@kookmin.ac.kr>
2021-02-02 14:50:24 +0900

    스크린샷 프로그램 개선

--- image_merge/auto_screenshot.py
+++ image_merge/auto_screenshot.py
@@ -1,11 +1,14 @@
+import keyboard
 import time
-
 # 파이썬 이미지 라이브러리
 from PIL import ImageGrab
 
-time.sleep(5) # 5초 대기
-
-for i in range(1, 11): # 1 ~ 10 2초간격
+def screenshot():
+    curr_time = time.strftime("_%Y%m%d_%H%M%S")
     img = ImageGrab.grab() #현재 스크린의 이미지를 가져옴
-    img.save("image{}.png".format(i)) #파일로 저장
-    time.sleep(2) # 2초 대기
+    img.save("image{}.png".format(curr_time))  # 파일로
+
+keyboard.add_hotkey("F9", screenshot) # 사용자가 f9를 누르면 스크린샷 저장
+keyboard.add_hotkey("ctrl+shift+s", screenshot) # 사용자가 컨트롤+쉬프트+S를 누르면 스크린샷 저장
+
+keyboard.wait("esc") # 사용자가 esc를 누를때 까지 프로그램 수행
