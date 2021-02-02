5df841dea6500315bc21808d2a36fde5d2e0fc84
JoSangYeon <whtkddus98@kookmin.ac.kr>
2021-02-02 14:06:46 +0900

    python zip() 사용법

---
+++ widget/15_zip.py
@@ -0,0 +1,11 @@
+kor = ["사과", "바나나", "오렌지"]
+eng = ["apple", "bananan", "orange"]
+
+print(list(zip(kor, eng))) # 압축
+
+mixed = [('사과', 'apple'), ('바나나', 'bananan'), ('오렌지', 'orange')]
+print(list(zip(*mixed))) # 해제
+
+kor2, eng2 = list(zip(*mixed))
+print(kor2)
+print(eng2)
