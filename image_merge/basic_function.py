88fad1e61e8d3b3711614efff26afba1cde0a7e6
JoSangYeon <whtkddus98@kookmin.ac.kr>
2021-02-02 14:09:49 +0900

    function 구현
    각종 버튼에 대한 함수 구현

--- image_merge/basic_function.py
+++ image_merge/basic_function.py
@@ -1,19 +1,84 @@
 from tkinter import * #티킨터 모듈에 있는 함수를 사용
 import tkinter.ttk as ttk
+from tkinter import filedialog # 로컬 피시에 있는 파일을 불러올 수 있는 모듈
+import tkinter.messagebox as msgbox
 
 """기본 프레임 설정"""
 root = Tk() # tk창 생성
 root.title("JSY GUI") # 제목 설정
 root.resizable(False, False) #창 크기 고정
 
+"""파일 추가 함수"""
+def add_file():
+    """
+    filedialog.askopenfilenames(): 복수개의 파일을 선택하도록 함
+        title : 제목
+        filetypes : 파일의 확장자를 설정(튜플 형태로)
+        initialdir : 최초의 경로를 설정
+        return : Tuple
+    """
+    files = filedialog.askopenfilenames(title="이미지 파일을 선택하세요",
+            filetypes=(("PNG 파일", "*.png"), ("모든파일", "*.*")),
+            initialdir=r"C:\Users\조상연\Pictures\Saved Pictures")
+
+    # 사용자가 선택한 파일들을 목록에 추가함
+    for file in files:
+        list_file.insert(END, file) #list_file : listbox
+
+"""선택 삭제 함수"""
+def del_file():
+    """
+    Listbox.curselection()를 통해서 삭제할 사진들을 선택하고 뒤에서부터 삭제함
+    :return:
+    """
+    # list_file에 선택된 사진들
+    del_list = list_file.curselection()
+    
+    # 선택된 파일들을 삭제함
+    for idx in reversed(del_list):
+        list_file.delete(idx)
+
+"""저장 경로 설정 함수"""
+def browse_dest_path():
+    """
+    합병한 이미지를 저장할 경로를 설정하는 함수
+    :return:
+    """
+    folder_selected = filedialog.askdirectory(title="저장할 경로를 설정하세요") #폴더를 선택하는 창을 띙움
+
+    if folder_selected == None:
+        return
+    else:
+        txt_dest_path.delete(0, END)
+        txt_dest_path.insert(0, folder_selected)
+
+"""이미지 합병 시작 버튼 함수"""
+def start():
+    """
+    :return: 
+    """
+    # 각 옵션들 값을 확인
+    print("가로넓이 :", cmb_width.get())
+    print("간격 : ", cmb_space.get())
+    print("포맷 : ", cmb_format.get())
+
+    # 파일 목록 확인
+    if list_file.size() == 0:
+        msgbox.showwarning("경고", "이미지 파일을 추가하세요!")
+        return
+    # 저장 경로 확인
+    if len(txt_dest_path.get()) == 0:
+        msgbox.showwarning("경고", "저장 경로를 선택하세요!")
+        return
+
 """파일 프레임 (파일 추가, 선택 삭제)"""
 file_frame = Frame(root)
 file_frame.pack(fill="x", padx=5, pady=5)
 
-btn_add_file = Button(file_frame, text="파일추가", padx=5, pady=5, width=10)
+btn_add_file = Button(file_frame, text="파일추가", padx=5, pady=5, width=10, command=add_file)
 btn_add_file.pack(side="left")
 
-btn_del_file = Button(file_frame, text="선택삭제", padx=5, pady=5, width=10)
+btn_del_file = Button(file_frame, text="선택삭제", padx=5, pady=5, width=10, command=del_file)
 btn_del_file.pack(side="right")
 
 """리스트 프레임"""
@@ -23,7 +88,7 @@ list_frame.pack(fill="both", padx=5, pady=5)
 scroll = Scrollbar(list_frame)
 scroll.pack(side="right", fill="y")
 
-list_file = Listbox(list_frame, selectmode="extemded", height=15, yscrollcommand=scroll.set)
+list_file = Listbox(list_frame, selectmode="extended", height=15, yscrollcommand=scroll.set)
 list_file.pack(side="left", fill="both", expand=True)
 
 scroll.config(command=list_file.yview)
@@ -35,7 +100,7 @@ path_frame.pack(fill="x", padx=5, pady=5, ipady=5)
 txt_dest_path = Entry(path_frame)
 txt_dest_path.pack(side="left", fill="x", expand=True, ipady=4, padx=5, pady=5) # ipady = 내부 높이변경
 
-btn_dest_path = Button(path_frame, text="찾아보기", width=10)
+btn_dest_path = Button(path_frame, text="찾아보기", width=10, command=browse_dest_path)
 btn_dest_path.pack(side="right", padx=5, pady=5)
 
 """옵션 프레임"""
@@ -88,7 +153,7 @@ frame_run.pack(fill="x", padx=5, pady=5)
 btn_close = Button(frame_run, padx=5, pady=5, text="닫기", width=12, command=root.quit)
 btn_close.pack(side="right", padx=5, pady=5)
 
-btn_start = Button(frame_run, padx=5, pady=5, text="시작", width=12)
+btn_start = Button(frame_run, padx=5, pady=5, text="시작", width=12, command=start)
 btn_start.pack(side="right", padx=5, pady=5)
 
 """메인 창 실행"""
