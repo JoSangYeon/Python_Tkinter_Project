0ef5c820932fe8c82ba04f004efc57de8b568500
JoSangYeon <whtkddus98@kookmin.ac.kr>
2021-02-02 14:08:49 +0900

    자동 스크린샷 프로그램 작성

---
+++ image_merge/auto_screenshot.py
@@ -0,0 +1,11 @@
+import time
+
+# 파이썬 이미지 라이브러리
+from PIL import ImageGrab
+
+time.sleep(5) # 5초 대기
+
+for i in range(1, 11): # 1 ~ 10 2초간격
+    img = ImageGrab.grab() #현재 스크린의 이미지를 가져옴
+    img.save("image{}.png".format(i)) #파일로 저장
+    time.sleep(2) # 2초 대기
